b1204666a3442bd7d74ead67cdcd4d3863fc54b5
Gustavo Lopes Noll <48497060+GustavoNoll@users.noreply.github.com>
2025-08-18 15:09:10 -0300

    a

--- src/lib/chart-projections.ts
+++ src/lib/chart-projections.ts
@@ -378,9 +378,12 @@ export function generateProjectionData(
         return null;
       }
 
+      console.log(context.limitAgeDate);
+      console.log(year);
+      console.log(currentMonthNumber);
       // Skip months after the limit age date
-      if (context.limitAgeDate && year >= context.limitAgeDate.getFullYear() ||
-        (year === context.limitAgeDate.getFullYear() && currentMonthNumber >= context.limitAgeDate.getMonth() + 1)) {
+      if (context.limitAgeDate && year > context.limitAgeDate.getFullYear() ||
+        (year === context.limitAgeDate.getFullYear() && currentMonthNumber > context.limitAgeDate.getMonth() + 1)) {
         return null;
       }
 
