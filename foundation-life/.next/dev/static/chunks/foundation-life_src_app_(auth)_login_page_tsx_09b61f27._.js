929a8090e4b29cb9c1d5fc7682b36049b72130b9
Gustavo Lopes Noll <48497060+GustavoNoll@users.noreply.github.com>
2026-02-17 22:10:22 -0300

    a

--- foundation-life/.next/dev/static/chunks/foundation-life_src_app_(auth)_login_page_tsx_09b61f27._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/1fc37_dc7a5073._.js",
-  "static/chunks/foundation-life_src_91acd86b._.js"
-],
-    source: "dynamic"
-});
--- foundation-life/.next/dev/static/chunks/foundation-life_src_app_layout_tsx_98791d7c._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/foundation-life_src_app_globals_0e5f2ffa.css",
-  "static/chunks/1fc37_5aeaf723._.js",
-  "static/chunks/foundation-life_src_36729f67._.js"
-],
-    source: "dynamic"
-});
